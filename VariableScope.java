363a7f4c0db5d09fcdfaaadaea9ebd54fe31253c
waltermirabuenos <196752274+waltermirabuenos@users.noreply.github.com>
2025-02-25 15:14:23 +0800

    Submitting OBJPROG Week 05 - Laboratory # 15

---
+++ VariableScope.java
@@ -0,0 +1,21 @@
+public class VariableScope {
+    // Global variable
+    static int globalCount = 100;
+
+    public static void main(String[] args) {
+        
+        System.out.println("Global Count (from main): " + globalCount);
+        
+        
+        showScope();
+    }
+
+    public static void showScope() {
+    
+        int localCount = 50;
+        
+        
+        System.out.println("Global Count (from showScope): " + globalCount);
+        System.out.println("Local Count (from showScope): " + localCount);
+    }
+}
